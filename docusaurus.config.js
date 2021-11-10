4421f7a93ccfa7537486f24494a53b06bd3cfecb
原田 怜奈 <91444135+Harada-Reina@users.noreply.github.com>
2021-11-10 19:40:41 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://harada-reina.github.io',
-  baseUrl: '/',
+  baseUrl: '/SampleDoc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
